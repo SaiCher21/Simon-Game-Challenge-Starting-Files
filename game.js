a34f15efbfe119cb7540176776aa229d8d17e55d
krischer89 <cherukurikrishnakishore@gmail.com>
2024-02-28 07:25:36 +0530

    r

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-var buttonColours = new Array["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
 
@@ -6,23 +7,30 @@ var started = false;
 var level = 0;
 
 $(document).keypress(function () {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
 });
 
 
-$("button").click(function () {
+$(".btn").click(function () {
+
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
     checkAnswer(userClickedPattern.length - 1);
 });
 
 
 function checkAnswer(currentLevel) {
+
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        
         console.log("success");
 
         if (userClickedPattern.length === gamePattern.length) {
@@ -33,11 +41,13 @@ function checkAnswer(currentLevel) {
     } else {
         console.log("wrong");
         playSound("wrong");
-        $(body).addClass("game-over");
+        $("body").addClass("game-over");
         setTimeout(function () {
-            $(body).removeClass("game-over");
+            $("body").removeClass("game-over");
         }, 200);
+
         $("#level-title").text("Game Over, Press Any Key to Restart");
+        
         startOver();
     }
 }
@@ -45,19 +55,20 @@ function checkAnswer(currentLevel) {
 
 function nextSequence() {
     userClickedPattern = [];
-    
     level++;
     $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4) + 1;
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour)
 }
 
 
 function playSound(name) {
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
